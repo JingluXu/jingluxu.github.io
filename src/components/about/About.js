5b2d407811ed545dff25dd25cb61664b5e3dc331
JingluXu <mixlulu@gmail.com>
2024-07-06 19:51:41 +1200

    updated CV

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -94,10 +94,10 @@ const skills = [
   {
     id: 1,
     description: [
-      { text: "Guidewire ClaimCenter", id: 1 },
       { text: "RESTful API", id: 2 },
       { text: "AWS", id: 3 },
       { text: "Kafka", id: 4 },
+      { text: "Guidewire Cloud Platform", id: 1 }
     ],
   },
 ];
@@ -158,7 +158,7 @@ export const About = () => {
           <div className={classes.intro}>
             <Typography gutterBottom variant="body1">HELLO! 你好! KIA ORA!</Typography><br />
             <Typography gutterBottom variant="body1">I have over four years of experience in designing, developing, and maintaining software applications in several large organizations.
-              Currently, I am working as a Guidewire ClaimCenter Developer at Insurance Australia Group.</Typography><br />
+              Currently, I am working as a Software Engineer at Insurance Australia Group.</Typography><br />
             <Typography gutterBottom variant="body1">Here are a few technologies I've been working with recently:</Typography>
             <Grid container spacing={4}>
               <Grid item md={6}>
